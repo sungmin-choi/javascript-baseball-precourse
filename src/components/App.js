ccfa1808a067234d3bc8d0c5dac2ff41b233c8af
sungmin-choi <namja306@naver.com>
2021-10-31 17:50:54 +0900

    Feat) 입력숫자 검증함수 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import { ID } from "../utils/constants.js";
 import { generateComputerNumber } from "../utils/games.js";
+import { validationInputNumber } from "../utils/validations.js";
 export default function BaseballGame(){
     const $app=document.getElementById(ID.APP);
     const $submit = document.getElementById(ID.SUBMIT);
     const $userInput=document.getElementById(ID.USER_INPUT);
 
     const computerInputNumbers = generateComputerNumber();
-
     console.log(`🎁this is answer:${computerInputNumbers}`);
 
     const userInputNumber=(e)=>{
         e.preventDefault();
-        console.log($userInput.value);
+        const userInputNumbers = $userInput.value.split('').map(e=>+e);
+        if(validationInputNumber(userInputNumbers)){
+            console.log("good");
+
+        }else{
+            alert("😢입력이 잘못됬습니다 다시 입력해주세요.");
+        }
     }
     $submit.addEventListener("click",userInputNumber);
 
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,3 +1,26 @@
-export default function validationInputNumber(userInputNumber){
+const validationInputNumber=(userInputNumbers)=>{
+    if(checkInputNumberLength(userInputNumbers) &&
+        checkInputNumberDiferrent(userInputNumbers) &&
+        checkInputNumberIncludeZero(userInputNumbers)) return true;
+    else return false;
 
 }
+
+const checkInputNumberLength=(userInputNumbers)=>{
+    if(userInputNumbers.length===3)return true;
+    else return false;
+}
+const checkInputNumberDiferrent=(userInputNumbers)=>{
+    const [num1,num2,num3]=userInputNumbers;
+    if(num1!==num2 && num1!==num3 && num2!==num3) return true;
+    else return false;
+
+}
+const checkInputNumberIncludeZero=(userInputNumbers)=>{
+    if(userInputNumbers.indexOf(0)===-1)return true;
+    else return false;
+
+}
+
+
+export {validationInputNumber}
