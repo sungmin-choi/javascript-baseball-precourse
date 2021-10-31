9e4707bc88e553e17b9bf354d3f5af8b4f26c845
sungmin-choi <namja306@naver.com>
2021-10-31 19:20:22 +0900

    Feat) 다시하기 함수 추가 resetGame

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import { ID,MESSAGE} from "../utils/constants.js";
+import { ID,MESSAGE,NUMBER} from "../utils/constants.js";
 import { generateComputerNumber,getHint,getHintMessage } from "../utils/games.js";
 import { validationInputNumber } from "../utils/validations.js";
 export default function BaseballGame(){
+    let [ball,strike]=[0,0];
     const $app=document.getElementById(ID.APP);
     const $submit = document.getElementById(ID.SUBMIT);
     const $userInput=document.getElementById(ID.USER_INPUT);
     const $resultView=document.getElementById(ID.RESULT_VIEW);
 
+    let computerInputNumbers = generateComputerNumber();
+    
 
-    const computerInputNumbers = generateComputerNumber();
-    console.log(`${MESSAGE.COMPUTER_NUMBER} ${computerInputNumbers}`);
     const play=(computerInputNumbers,userInputNumbers)=>{
-        const [ball,strike]=getHint(computerInputNumbers,userInputNumbers);
-        if(strike===3){
+        [ball,strike]=getHint(computerInputNumbers,userInputNumbers);
+        if(strike===NUMBER.THREE){
             return `<p>${MESSAGE.CORRECT}</p>
                     <span>${MESSAGE.RESTART}</span> <button id="game-restart-button">재시작</button>`;
         }else{
@@ -26,9 +27,20 @@ export default function BaseballGame(){
         if(validationInputNumber(userInputNumbers)){
             const message=play(computerInputNumbers,userInputNumbers);
             $resultView.innerHTML=`${message}`;
+            if(strike===NUMBER.THREE){
+            const $restart = document.getElementById(ID.RESTART);
+            $restart.addEventListener("click",resetGame);
+            }
         }else{
             alert(MESSAGE.WRONG_ALERT);
         }
+    }
+
+    const resetGame=(e)=>{
+        e.preventDefault();
+        computerInputNumbers = generateComputerNumber();
+        $resultView.innerHTML=``;
+        $userInput.value=``;
     }
     $submit.addEventListener("click",userInputNumber);
 
--- src/utils/games.js
+++ src/utils/games.js
@@ -6,6 +6,7 @@ const getRandomNumber=()=>{
 const generateComputerNumber=()=>{
     let computerNumber = new Set();
     while(computerNumber.size<NUMBER.THREE) computerNumber.add(getRandomNumber());
+    console.log(`${MESSAGE.COMPUTER_NUMBER} ${[...computerNumber]}`);
     return [...computerNumber];
 }
 
@@ -25,6 +26,7 @@ const getHintMessage=(ball,strike)=>{
     else if(ball===NUMBER.ZERO && strike>NUMBER.ZERO && strike<NUMBER.THREE) return `${strike}${MESSAGE.STRIKE}`;
     else if(ball>NUMBER.ZERO && strike>NUMBER.ZERO) return `${ball}${MESSAGE.BALL} ${strike}${MESSAGE.STRIKE}`
 }
+
 
 
 export {generateComputerNumber,getHint,getHintMessage};
